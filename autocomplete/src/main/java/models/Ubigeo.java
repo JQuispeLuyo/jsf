42f705a9ab0d0aee5f8fc1410da6097c4342949b
José Luis Quispe Luyo <jose.quispe@vallegrande.edu.pe>
2020-06-29 19:12:26 -0500

    modelo

---
+++ autocomplete/src/main/java/models/Ubigeo.java
@@ -0,0 +1,44 @@
+package models;
+
+import java.io.Serializable;
+
+public class Ubigeo implements Serializable {
+    
+    private String CODUBI;
+    private String NOMDEP;
+    private String NOMPROV;
+    private String NOMDIST;
+
+    public String getCODUBI() {
+        return CODUBI;
+    }
+
+    public void setCODUBI(String CODUBI) {
+        this.CODUBI = CODUBI;
+    }
+
+    public String getNOMDEP() {
+        return NOMDEP;
+    }
+
+    public void setNOMDEP(String NOMDEP) {
+        this.NOMDEP = NOMDEP;
+    }
+
+    public String getNOMPROV() {
+        return NOMPROV;
+    }
+
+    public void setNOMPROV(String NOMPROV) {
+        this.NOMPROV = NOMPROV;
+    }
+
+    public String getNOMDIST() {
+        return NOMDIST;
+    }
+
+    public void setNOMDIST(String NOMDIST) {
+        this.NOMDIST = NOMDIST;
+    }
+    
+}
